c51c06058762c3ce328723c057c2c831c98657f0
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-03 19:47:56 -0300

    atividade de sei lá oq

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Minhas frutas favoritas");
+
+Console.WriteLine("jabuticaba");
+Console.WriteLine("fruta do conde");
+Console.WriteLine("caqui");
+Console.WriteLine("maracujá");
+Console.WriteLine("abacate");
